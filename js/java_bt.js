117563cd537e6cf8a1fe3fb9e9c235c2c3d6aa33
‘morganelcvz’ <morgane.lecalvez@outlook.fr>
2024-12-11 10:03:31 +0100

    ajout exercices terminés

--- js/java_bt.js
+++ js/java_bt.js
@@ -3,14 +3,35 @@ document.getElementById("showbutton").addEventListener("click", function () {
     document.getElementById("display-toto").innerText = texte
 })
 
+//document.getElementById("btnR").addEventListener("click", function () {
+//    document.getElementById("display-toto").style.color = 'red'
+//})
+
+//document.getElementById("btnG").addEventListener("click", function () {
+//    document.getElementById("display-toto").style.color = 'green'
+//})
+
+//document.getElementById("btnB").addEventListener("click", function () {
+//    document.getElementById("display-toto").style.color = 'blue'
+//})
+
 document.getElementById("btnR").addEventListener("click", function () {
-    document.getElementById("display-toto").style.color = 'red'
+    document.querySelector("#display-toto").removeAttribute("class")
+    document.querySelector("#display-toto").classList.add("text-danger")
 })
 
 document.getElementById("btnG").addEventListener("click", function () {
-    document.getElementById("display-toto").style.color = 'green'
+    document.querySelector("#display-toto").removeAttribute("class")
+    document.querySelector("#display-toto").classList.add("text-success")
 })
 
 document.getElementById("btnB").addEventListener("click", function () {
-    document.getElementById("display-toto").style.color = 'blue'
+    document.querySelector("#display-toto").removeAttribute("class")
+    document.querySelector("#display-toto").classList.add("text-primary")
+})
+
+
+document.getElementById("cancelbutton").addEventListener("click", function () {
+    let texte = document.getElementById("totoro-text").value = ""
+    document.getElementById("display-toto").innerText = ""
 })
